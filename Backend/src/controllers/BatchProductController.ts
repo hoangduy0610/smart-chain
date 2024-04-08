6573de357d1e7f473db5e9a036679d6fd6e592a7
Nguyễn Hoàng Duy <45957687+hoangduy0610@users.noreply.github.com>
2024-04-08 22:19:25 +0900

    feat: Batch module (#18)

---
+++ Backend/src/controllers/BatchProductController.ts
@@ -0,0 +1,56 @@
+import { Body, Controller, Delete, Get, Param, Post, Put, Query, Req, Res, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { EnumRoles } from 'src/commons/EnumRoles';
+import { CreateBatchProductDto, EditBatchProductDto } from 'src/dtos/BatchProductDtos';
+import { Roles } from 'src/guards/RoleDecorator';
+import { RolesGuard } from 'src/guards/RoleGuard';
+import { BatchProductInterfaces } from 'src/interfaces/BatchProductInterfaces';
+import { BatchProductService } from 'src/services/BatchProductService';
+
+@ApiTags('batch-product')
+@Controller('batch-product')
+export class BatchProductController {
+    constructor(private readonly batchProductService: BatchProductService) { }
+
+    @Get('/list')
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @ApiBearerAuth()
+    @Roles(EnumRoles.ROLE_ADMIN, EnumRoles.ROLE_FARMER)
+    async list(@Req() req, @Res() res) {
+        return res.status(200).json(await this.batchProductService.findAllByRoleAndId(req.user.role, req.user.id));
+    }
+
+    @Get('/:id')
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @ApiBearerAuth()
+    @Roles(EnumRoles.ROLE_ADMIN, EnumRoles.ROLE_FARMER)
+    async findOne(@Req() req, @Res() res, @Param('id') id: string) {
+        return res.status(200).json(await this.batchProductService.findById(id));
+    }
+
+    @Post('/')
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @ApiBearerAuth()
+    @Roles(EnumRoles.ROLE_ADMIN, EnumRoles.ROLE_FARMER)
+    async create(@Req() req, @Res() res, @Body() batchProductDto: CreateBatchProductDto) {
+        return res.status(200).json(await this.batchProductService.create(req.user.id, batchProductDto));
+    }
+
+    @Put('/:id')
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @ApiBearerAuth()
+    @Roles(EnumRoles.ROLE_ADMIN, EnumRoles.ROLE_FARMER)
+    async update(@Req() req, @Res() res, @Param('id') id: string, @Body() batchProductDto: EditBatchProductDto) {
+        return res.status(200).json(await this.batchProductService.update(id, batchProductDto));
+    }
+
+    @Delete('/:id')
+    @UseGuards(AuthGuard('jwt'), RolesGuard)
+    @ApiBearerAuth()
+    @Roles(EnumRoles.ROLE_ADMIN, EnumRoles.ROLE_FARMER)
+    async delete(@Req() req, @Res() res, @Param('id') id: string) {
+        console.log(req.user);
+        return res.status(200).json(await this.batchProductService.delete(req.user.username, id));
+    }
+}
--- Backend/src/services/BatchProductService.ts
+++ Backend/src/services/BatchProductService.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { BatchProductInterfaces } from '../interfaces/BatchProductInterfaces';
 import { BatchProductRepository } from '../repositories/BatchProductRepository';
+import { EnumRoles } from 'src/commons/EnumRoles';
+import { IdUtils } from 'src/utils/IdUtils';
+import { CreateBatchProductDto, EditBatchProductDto } from 'src/dtos/BatchProductDtos';
 
 @Injectable()
 export class BatchProductService {
     constructor(
         private readonly batchProductRepository: BatchProductRepository,
     ) { }
-    
-    async create(batchProduct: BatchProductInterfaces): Promise<BatchProductInterfaces> {
+
+    async create(owner: string, dto: CreateBatchProductDto): Promise<BatchProductInterfaces> {
+        const batchProduct = {
+            ...dto,
+            owner,
+            batchId: IdUtils.generateId(12),
+        }
         return await this.batchProductRepository.create(batchProduct);
     }
 
@@ -16,11 +24,26 @@ export class BatchProductService {
         return await this.batchProductRepository.findAll();
     }
 
+    async findAllByRoleAndId(role: string, id: string): Promise<BatchProductInterfaces[]> {
+        switch (role) {
+            case EnumRoles.ROLE_ADMIN:
+                return await this.batchProductRepository.findAll();
+            case EnumRoles.ROLE_FARMER:
+                return await this.batchProductRepository.findByOwnerId(id);
+            default:
+                return await this.batchProductRepository.findByOwnerId(id);
+        }
+    }
+
+    async findByOwnerId(ownerId: string): Promise<BatchProductInterfaces[]> {
+        return await this.batchProductRepository.findByOwnerId(ownerId);
+    }
+
     async findById(id: string): Promise<BatchProductInterfaces> {
         return await this.batchProductRepository.findById(id);
     }
 
-    async update(id: string, batchProduct: BatchProductInterfaces): Promise<BatchProductInterfaces> {
+    async update(id: string, batchProduct: EditBatchProductDto): Promise<BatchProductInterfaces> {
         return await this.batchProductRepository.update(id, batchProduct);
     }
 
