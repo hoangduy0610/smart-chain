d1c2171be15f4f504816cb44b2c18302ea8724b2
hieuuitttnt <114348338+hieuuitttnt@users.noreply.github.com>
2024-04-18 09:31:30 +0700

    thêm UD user (#19)

--- Frontend/js/apis.js
+++ Frontend/js/apis.js
@@ -1,6 +1,8 @@
-const API_HOST = 'https://dynamic-ladybird-centrally.ngrok-free.app';
+const API_HOST = 'https://hongdi.ddns.net';
 
 const API_ENDPOINT = {
     LOGIN: `${API_HOST}/auth/signin`,
     CREATE_USER: `${API_HOST}/user`,
+    READ_USER: `${API_HOST}/user/list`,
+    GET_USER: `${API_HOST}/user/`,
 }
