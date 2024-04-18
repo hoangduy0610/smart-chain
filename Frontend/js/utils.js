ba51278433be5081e2e773ebef68e6dbec74dd68
Nguyễn Hoàng Duy <45957687+hoangduy0610@users.noreply.github.com>
2024-04-18 12:45:52 +0900

    Improve/refactor phase 2 (#21)
    * improve: refactor
    
    * refactor 2
    
    * final refactor

--- Frontend/js/utils.js
+++ Frontend/js/utils.js
@@ -5,3 +5,68 @@ function getToken() {
     }
     return `Bearer ${localStorageToken}`;
 }
+
+/**
+ * 
+ * @param {*} endpoint 
+ * @param {*} params 
+ * @returns 
+ */
+function fillEndpointPlaceholder(endpoint, params) {
+    let result = endpoint;
+    for (const key in params) {
+        result = result.replace(`{{${key}}}`, params[key]);
+    }
+    // Example:
+    // fillEndpointPlaceholder(API_ENDPOINT.PRODUCT.GET_PRODUCT, { id: 1 })
+    return result;
+}
+
+const initDataTable = (selector, ajaxOptions, columns, columnDefs, callback) => {
+    return $(selector).DataTable({
+        "searching": true,
+        "paging": true,
+        "pagingType": "full_numbers",
+        "lengthMenu": [
+            [5, 10, 25, 50, 100, -1],
+            [5, 10, 25, 50, 100, "All"]
+        ],
+        "language": {
+            "lengthMenu": "Hiển thị _MENU_ giá trị mỗi trang",
+            "zeroRecords": "Không có bản ghi phù hợp",
+            "info": "Đang hiển thị trang thứ _PAGE_ trên _PAGES_ trang",
+            "infoEmpty": "Không có bản ghi nào",
+            "infoFiltered": "(Đã lọc được _MAX_ bản ghi)",
+            "search": "Tìm kiếm: ",
+            "paginate": {
+                "first": "Trang đầu",
+                "last": "Trang cuối",
+                "next": "Tiếp",
+                "previous": "Trước"
+            },
+        },
+        // Call API login here
+        "ajax": {
+            'url': ajaxOptions.ajaxUrl,
+            'type': 'GET',
+            'beforeSend': function (request) {
+                request.setRequestHeader("Authorization", ACCESS_TOKEN);
+            },
+            "dataSrc": ""
+        },
+        columnDefs: columnDefs || [],
+        "columns": columns.map((column) => {
+            if (typeof column === "string") {
+                return {
+                    data: column
+                }
+            }
+            return {
+                ...column
+            }
+        }),
+        "initComplete": function (settings, json) {
+            callback && callback(settings, json)
+        }
+    });
+}
