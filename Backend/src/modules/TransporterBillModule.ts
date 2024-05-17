402972df959f6518bce7a421c1cb5c565230b033
Nguyễn Hoàng Duy <45957687+hoangduy0610@users.noreply.github.com>
2024-05-17 20:34:59 +0700

    finish transporter bill (#35)

--- Backend/src/modules/TransporterBillModule.ts
+++ Backend/src/modules/TransporterBillModule.ts
@@ -4,12 +4,15 @@ import { TransporterBillRepository } from 'src/repositories/TransporterBillRepos
 import { TransporterBillSchema } from 'src/schemas/TransporterBillSchema';
 import { TransporterBillController } from '../controllers/TransporterBillController';
 import { TransporterBillService } from '../services/TransporterBillService';
+import { BatchProductRepository } from 'src/repositories/BatchProductRepository';
+import { BatchProductSchema } from 'src/schemas/BatchProductSchema';
 
 @Module({
     imports: [
         MongooseModule.forFeature([{ name: 'TransporterBill', schema: TransporterBillSchema }]),
+        MongooseModule.forFeature([{ name: 'BatchProduct', schema: BatchProductSchema }]),
     ],
-    providers: [TransporterBillService, TransporterBillRepository],
+    providers: [TransporterBillService, TransporterBillRepository, BatchProductRepository],
     controllers: [TransporterBillController],
     exports: [TransporterBillService, TransporterBillRepository]
 })
