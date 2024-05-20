93137ddea7cb8a399823722f1c7ce7305784411f
Nguyễn Hoàng Duy <45957687+hoangduy0610@users.noreply.github.com>
2024-05-20 17:44:34 +0700

    add api for scan (#39)
    Co-authored-by: khanh <khanh2003dakdoa@gmail.com>

--- ScanPage/js/para-details.js
+++ ScanPage/js/para-details.js
@@ -1,6 +1,60 @@
 $(document).ready(function () {
-     
-    $('.btn-back').click(function () {
-       window.location.href = 'scan.html'
-    })
- });
+
+   const params = new window.URLSearchParams(window.location.search);
+   const prefetch_id = params.get('id_batch')
+   $.ajax({
+      url: `https://hongdi.ddns.net/batch-product/scan/${prefetch_id}`,
+      method: 'GET',
+      headers: {
+      },
+      success: function (response) {
+         console.log("load data success!")
+         $('.detail-para-body').append(`
+               <div style ="font-weight: 700; font-size: 1.5rem !important; margin-bottom: 2px; ">${response[0].name}</div>
+               <div>Số lượng: <p style ="color: blue; display: inline; ">${response[0].quantity}</p></div>
+               <div>Giá bán: <p style ="color: blue; display: inline; ">${response[0].product[0].price} đ</p></div>
+               <div>Trạng thái hiện tại: <p style = "color: blue; display: inline; ">${response[0].status}</p></div>
+               <div  class="bar1"> </div>
+               <div style ="font-weight: 700; font-size: 1.5rem !important; margin-bottom: 2px; " class="descript">
+                    Mô Tả
+               </div>
+               <p style ="font-style: italic">${response[0].product[0].description}</p>
+               ${response[0].product[0].attributes.map((attr) => {
+               return showarr(attr)
+               }).join('')}
+               <div  class="bar1"> </div>
+               <div style ="font-weight: 700; font-size: 1.5rem !important; margin-bottom: 2px; " class="descript">
+                    Nhật ký
+               </div>
+               ${response[0].history.map((attr) => {
+                  return showhis(attr)
+                  }).join('')}
+            `)
+
+      },
+   })
+
+   $('.btn-back').click(function () {
+      window.location.href = 'scan.html'
+   })
+});
+
+function showarr(attr) {
+   var arr = []
+   arr.push(attr)
+   let result = '';
+   for (let i = 0; i < arr.length; i++) {
+     result += `<p>${arr[i].name} : <span style="display:inline; color: blue;">${arr[i].value}</span></p>`;
+   }
+   return result;
+ }
+
+ function showhis(attr) {
+   var arr = []
+   arr.push(attr)
+   let result = '';
+   for (let i = 0; i < arr.length; i++) {
+     result += `<p><i class="fa-solid fa-clock-rotate-left me-1"></i>${arr[i].action}</p>`;
+   }
+   return result;
+ }
