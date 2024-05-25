2090ce3eb79227c4070c812dd61709ef315903fb
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2024-05-25 13:27:09 +0700

    deploy

--- Frontend/js/apis.js
+++ Frontend/js/apis.js
@@ -1,5 +1,5 @@
 // const API_HOST = 'http://10.10.12.55:8798';
-const API_HOST = 'https://hongdi.ddns.net'
+const API_HOST = 'https://api.smcsoft.online'
 
 const QR_TARGET_WEB = 'https://smartchain.ddns.net/ScanPage/scan.html?id=';
 const QR_HOST = `https://api.qrserver.com/v1/create-qr-code/?data=${QR_TARGET_WEB}`;
--- README.md
+++ README.md
@@ -3,3 +3,9 @@
 ```sh
 docker-compose --env-file docker.env up -d --build
 ```
+
+# Restart all docker containers
+
+```sh
+docker restart $(docker ps -a -q)
+```
